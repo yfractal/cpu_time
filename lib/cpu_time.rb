ad7684490788d791b513d0ecd46a5dcd1d9a34e8
Mike Yang <yfractal@gmail.com>
2024-05-29 22:22:23 +0800

    init

---
+++ lib/cpu_time.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cpu_time/version"
+
+module CpuTime
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/cpu_time/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CpuTime
+  VERSION = "0.1.0"
+end
